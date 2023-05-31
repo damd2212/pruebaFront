3fc47d62db83db16a4a6ca794ba026afd24e931e
damd2212 <53495267+damd2212@users.noreply.github.com>
2023-05-31 09:11:22 -0500

    cambios agregados

--- prueba-app/src/app/components/listar-pruebas/listar-pruebas.component.ts
+++ prueba-app/src/app/components/listar-pruebas/listar-pruebas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Prueba } from 'src/app/models/prueba';
 import { PruebasService } from 'src/app/services/pruebas.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-pruebas',
@@ -22,8 +23,10 @@ export class ListarPruebasComponent implements OnInit{
   eliminarRegistro(id:number){
     this.pruebaService.deletePrueba(id).subscribe(res=>{
       if (res) {
+        Swal.fire('Prueba eliminada','La prueba con id ' + id + ' ha sido eliminada','success')
         console.log("Prueba eliminada");
       }else{
+        Swal.fire('Erro de eliminacion','Error al eliminar la prueba con id '+id,'error')
         console.log("Erro al eliminar la prueba");
       }
       this.ngOnInit()
